b787edd72a65bc0cc24728387c867ec9c435010f
Spencer <smtaylorslc@gmail.com>
2016-10-27 19:19:34 -0600

    test

---
+++ python-getting-started/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/spencertaylor/HEROKU_PORTFOLIO/python-getting-started/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
