61b33c9cefc0c8af82d8e7cf6387a7b3c2f54d32
Spencer Taylor <smtaylorslc@gmail.com>
2016-10-28 14:05:11 -0600

    Changes to be committed: 	modified:   gettingstarted/__init__.pyc 	modified:   gettingstarted/settings.py 	modified:   gettingstarted/settings.pyc 	modified:   gettingstarted/urls.pyc 	modified:   gettingstarted/wsgi.pyc 	modified:   hello/__init__.pyc 	modified:   hello/admin.pyc 	modified:   hello/models.pyc 	modified:   hello/templates/index.html 	new file:   hello/templates/static/style.css 	new file:   hello/templates/static/windows_xp_bliss-1280x720.jpg 	modified:   hello/views.py 	modified:   hello/views.pyc

--- python-getting-started2/hello/views.py
+++ python-getting-started2/hello/views.py
@@ -6,11 +6,8 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
-    r = requests.get('http://httpbin.org/status/418')
-    print r.text
-    return HttpResponse('<pre>' + r.text + '</pre>')
     # return HttpResponse('Hello from Python!')
-    #return render(request, 'index.html')
+    return render(request, 'index.html')
 
 
 def db(request):
